395817ea0126f6e18ad034c7542de09904bf35dd
Dinirhm <101705202+Dinirohmah@users.noreply.github.com>
2022-04-05 16:32:48 +0700

    Add files via upload

---
+++ 2.1 penambahan bil bulat.cpp
@@ -0,0 +1,18 @@
+/*Nama : Dini Rohmah
+  Nim : 2100018234
+  Kelas : E */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b,c,hsl;
+	
+	cout<<"program menghitung 3 buah bilangan bulat"<<endl;
+	cout<<"\nMasukkan nilai a : "; cin>>a;
+	cout<<"\nMasukkan nilai b : "; cin>>b;
+	cout<<"\nMasukkan nilai c : "; cin>>c;
+	cout<<endl;
+	hsl = a+b+c;
+	cout<<"Jumlah = "<<hsl<<endl;
+}
